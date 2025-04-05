6a252b5600b0658fb01c2dfe48c6765fb3a226b9
kyogeshsagar <155466016+kyogeshsagar@users.noreply.github.com>
2025-04-05 13:57:56 +0530

    Create task2.js

---
+++ task2.js
@@ -0,0 +1,31 @@
+let a = 10;
+let b = 5;
+let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let quotient = a / b;
+let remainder = a % b;
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+console.log("Remainder:", remainder);
+
+
+let age = 18;
+
+if (age >= 18) {
+    console.log("You are eligible to vote.");
+} else {
+    console.log("You are not eligible to vote.");
+}
+
+
+function checkEvenOrOdd(number) {
+    if (number % 2 === 0) {
+        console.log(number + " is Even.");
+    } else {
+        console.log(number + " is Odd.");
+    }
+}
+checkEvenOrOdd(7); 
